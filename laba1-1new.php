a8087aa3456c48401ac2adb19f6ca1dd7d3df7bc
Nikita Rodiukov <zazaazzaaaaz@gmail.com>
2022-10-11 16:56:35 +0300

    fix(lw0): форматировал

--- laba1-1new.php
+++ laba1-1new.php
@@ -1,29 +1,29 @@
 <?php
 function calc(string $primer): int
 {
-	$arrZnaki = ['!', '@', '#', '%' . '^', '&', '_', '?', '$', '='];
-	echo $primer . "<br>";
-	echo var_dump($primer) . "<br>";
-	$arr = str_split($primer);
-	echo "<br>";
-	print_r($arr);
-	echo "<br>";
-	$arrLength = count($arr); //9;
-	for ($i = 0; $i < $arrLength; $i++) {
-		if (in_array($arr[$i], $arrZnaki)) {
-			unset($arr[$i]);
-		}
-		while (ctype_alpha($arr[$i])) {
-			unset($arr[$i]);
-		}
-	}
-	print_r($arr);
-	echo "<br>";
-	$arrDump = var_dump(implode($arr));
-	$arrNew = implode($arr);
-	echo $arrDump . "<br>";
-	echo $arrNew . "<br>";
-	return eval("return $arrNew;");
+    $arrZnaki = ['!', '@', '#', '%' . '^', '&', '_', '?', '$', '='];
+    echo $primer . "<br>";
+    echo var_dump($primer) . "<br>";
+    $arr = str_split($primer);
+    echo "<br>";
+    print_r($arr);
+    echo "<br>";
+    $arrLength = count($arr); //9;
+    for ($i = 0; $i < $arrLength; $i++) {
+        if (in_array($arr[$i], $arrZnaki)) {
+            unset($arr[$i]);
+        }
+        while (ctype_alpha($arr[$i])) {
+            unset($arr[$i]);
+        }
+    }
+    print_r($arr);
+    echo "<br>";
+    $arrDump = var_dump(implode($arr));
+    $arrNew = implode($arr);
+    echo $arrDump . "<br>";
+    echo $arrNew . "<br>";
+    return eval("return $arrNew;");
 }
 echo calc("!?1aadattggssf+2adada-?4=*7/#1a=&");
 ?>
