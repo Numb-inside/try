2642c0ed03103d0b22b63b0b5ab49b9e510e72d3
Nikita Rodiukov <zazaazzaaaaz@gmail.com>
2022-10-11 17:05:01 +0300

    fix(lw0): убрал лишний код

--- laba1-1new.php
+++ laba1-1new.php
@@ -2,12 +2,7 @@
 function calc(string $primer): int
 {
     $arrZnaki = ['!', '@', '#', '%' . '^', '&', '_', '?', '$', '='];
-    echo $primer . "<br>";
-    echo var_dump($primer) . "<br>";
     $arr = str_split($primer);
-    echo "<br>";
-    print_r($arr);
-    echo "<br>";
     $arrLength = count($arr); //9;
     for ($i = 0; $i < $arrLength; $i++) {
         if (in_array($arr[$i], $arrZnaki)) {
@@ -17,26 +12,8 @@ function calc(string $primer): int
             unset($arr[$i]);
         }
     }
-    print_r($arr);
-    echo "<br>";
-    $arrDump = var_dump(implode($arr));
     $arrNew = implode($arr);
-    echo $arrDump . "<br>";
-    echo $arrNew . "<br>";
     return eval("return $arrNew;");
 }
 echo calc("!?1aadattggssf+2adada-?4=*7/#1a=&");
 ?>
-<!-- 
-------------------------------------------------------------------------старый код
-function calc($primer)
-{
-    $resultatik = eval("return $primer;");
-    if (is_string($resultatik)) {
-        echo "Ошибочка";
-    } else {
-        echo $resultatik;
-    }
-}
-calc("5.6*4.5");
--->
