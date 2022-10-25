7a0ba59ef106e4a8278ac14f4abddc4ab45558a4
Nikita Rodiukov <zazaazzaaaaz@gmail.com>
2022-10-25 18:51:43 +0300

    initial commit

--- laba1-2new.php
+++ laba1-2new.php
@@ -4,6 +4,7 @@ function sumTime(string $timeFirst, string $timeSecond): string
     $firstMassive = [];
     $secondMassive = [];
     $arrZnaki = ['!', '@', '#', '%' . '^', '&', '_', '?', '$']; // /0
+    echo $timeFirst . "<br>", $timeSecond;
     $arrFirst = str_split($timeFirst);
     $arrSecond = str_split($timeSecond);
     $arrLength = count(array_merge($arrFirst, $arrSecond));
@@ -21,8 +22,11 @@ function sumTime(string $timeFirst, string $timeSecond): string
             unset($arrSecond[$i]);
         }
     }
+    echo "<br>";
     $timeFirst = implode($arrFirst);
     $timeSecond = implode($arrSecond);
+    echo $timeFirst . "<br>";
+    echo $timeSecond . "<br>";
     $firstMassive = explode(":", $timeFirst);
     $secondMassive = explode(":", $timeSecond);
     $firstSumm = $firstMassive[0] + $secondMassive[0];
@@ -38,8 +42,29 @@ function sumTime(string $timeFirst, string $timeSecond): string
     }
     while ($firstSumm > 23) {
         $firstSumm = $firstSumm - 24;
+        // $days = $days + 1;
     }
     return $firstSumm . ":" . $secondSumm . ":" . $thirdSumm . "<br>"; //12:50:44
 }
 echo  sumTime("5!0:adad3dad!#adad0adad:3!1", "1!0:2adada0:1$3");
 ?>
+<!--   /* 
+    ------------------------------------------------------------------------здесь происходит счет дней, неделей и месяцев just for fun!
+    
+	$days = 1;
+    $weeks = 1;
+    $months = 1;
+    while ($days > 7) {
+        $weeks = $weeks + 1;
+        $days = $days - 7;
+    }
+    while ($weeks > 4) {
+        $months = $months + 1;
+        $weeks = $weeks - 4;
+    }
+    echo $days . "-й день<br>";
+    echo $weeks . "-я неделя<br>";
+    echo $months . "-й месяц<br>"; */ 
+	
+	
+-->
---
+++ lw1/resources/views/home.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <div class="Logo">
+                <div class="container">
+                    <div class="logo_logo">
+                        <img src="{{ asset('kraska.png') }}"> KRASKAAAAAAAAAAAAAAAA
+                    </div>
+                    <div class="info">
+                        <div class="info_item">
+                            <div class="info_img"><img src="{{ asset('kraska.png') }}"></div>
+                            <div class="info_glavn">
+                                <div class="main_title">Color Documentation</div>
+                                <div class="main_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quidem distinctio iusto? Vel nulla deserunt labore, dolorum amet possimus aliquid.</div>
+                            </div>
+                        </div>
+                        <div class="info_item">
+                            <div class="img"><img src="{{ asset('kraska.png') }}"></div>
+                            <div class="info_glavn">
+                                <div class="main_title">Colorcast</div>
+                                <div class="main_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nam perspiciatis eveniet? Repellat saepe pariatur labore provident ducimus magnam consequuntur.</div>
+                            </div>
+                        </div>
+                        <div class="info_item">
+                            <div class="img"><img src="{{ asset('kraska.png') }}"></div>
+                            <div class="info_glavn">
+                                <div class="main_title">Color news</div>
+                                <div class="main_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, nulla! Maxime ipsam dolores iure accusantium praesentium incidunt deserunt dolore natus?</div>
+                            </div>
+                        </div>
+                        <div class="info_item">
+                            <div class="img"><img src="{{ asset('kraska.png') }}"></div>
+                            <div class="info_glavn">
+                                <div class="main_title">Color ecosytem</div>
+                                <div class="main_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, temporibus ipsa quod odit nostrum doloremque doloribus eius quasi laudantium exercitationem!</div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="trash">
+                        <div class="first_trash trashcan">
+                                <div class="shop"><img src="{{ asset('kraska.png') }}">Shop</div>
+                                <div class="sponsor"><img src="{{ asset('kraska.png') }}">Sponsor</div>
+                        </div>
+                        <div class="second_trash trashcan">
+                            <div class="trash_built">Built v8.83.25</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
